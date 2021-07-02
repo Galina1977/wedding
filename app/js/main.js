e2863bf9b9f57d14e9d20cef50bef12ea8caca29
Galina1977-dev <galyapener@mail.ru>
2021-07-02 21:36:28 +0500

    start project

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  // Слайдер-карусель Обычный
+  // $(".slider__inner").slick({
+  //   infinite: true,
+  //   fade: true,
+  //   prevArrow: '<button type="button" class="slick-prev"></button>',
+  //   nextArrow: '<button type="button" class="slick-next"></button>',
+  // });
+
+  // Анимация
+  new WOW().init();
+});
